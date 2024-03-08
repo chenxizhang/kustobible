d85e48a5aff451b262b1090c1d9c86d61422ef73
chenxizhang <ares@xizhang.com>
2024-03-08 17:22:51 +0800

    use jsdelivr CDN

--- scripts/process.js
+++ scripts/process.js
@@ -2,6 +2,13 @@
 const fs = require('fs');
 const path = require('path');
 
+// read book.json from the root folder
+const bookJsonPath = path.join(__dirname, '../book.json');
+const bookJson = require(bookJsonPath);
+
+// read the imagecdnprefix from the book.json
+const imageCdnPrefix = bookJson.imagecdnprefix;
+
 const docsPath = path.join(__dirname, '../docs');
 const regex = /!\[.*?\]\(((?!http).*?\/)?([^\/)]+)\)/g;
 
@@ -21,7 +28,7 @@ fs.readdir(docsPath, { recursive: true }, (err, files) => {
             }
 
             const result = data.replace(regex, (match, p1, p2) => {
-                return `![](https://cdn.jsdelivr.net/gh/chenxizhang/kustobible@master/docs/images/${p2})`;
+                return `![](${imageCdnPrefix}${p2})`;
             });
 
             fs.writeFile(filePath, result, 'utf-8', (err) => {
